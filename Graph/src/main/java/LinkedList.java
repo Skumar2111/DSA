43916761ccdd0ecd27a21c7b282de681004f67b6
Sushant Kumar <sushant.k052@gmail.com>
2025-01-16 23:20:11 +0530

    DFS Commit

---
+++ Graph/src/main/java/LinkedList.java
@@ -0,0 +1,35 @@
+public class LinkedList {
+
+    Node head;
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList lList = new LinkedList();
+
+        lList.head = new Node(10);
+        Node second = new Node(20);
+        Node thirdNode = new Node(30);
+
+        lList.head.next = second;
+        second.next = thirdNode;
+        thirdNode.next = null;
+
+
+
+
+
+    }
+
+}
+
